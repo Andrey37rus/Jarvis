d8bd80a00884add384fc2de064c61dcb37a7ee09
andrey <deman3796@mail.ru>
2022-10-27 09:07:36 +0300

    changes

--- reproduction.py
+++ reproduction.py
@@ -1,21 +1,43 @@
 import pyautogui
+import pyautogui as pg
 
 
-def up():
-    # x = 7
-    # a = (x//2)
-    pyautogui.press('volumeup', 7)
+def up(*args):
+    """делает звук громче в x2"""
+
+    pg.press(['volumeup'], 7)
 
 
-def down():
-    # x = 7
-    # a = (x//2)
-    pyautogui.press('volumedown', 7)
+def down(*args):
+    """делает звук тише в x2"""
+
+    pg.press(['volumedown'], 7)
 
 
-def soundless():
-    pyautogui.press('volumedown', 55)
+def soundless(*args):
+    """Делает комп без звука, со звуком"""
+
+    pg.press(['volumemute'])
+    return
 
 
-def sound():
-    pyautogui.press('volumeup', 15)
+def next_trak(*args):
+    """Включает следующий трек на компе"""
+
+    pg.press(['nexttrack'])
+
+
+def previous_track(*args):
+    """Включает предыдущий трек на компе"""
+    pg.press(['prevtrack'])
+    pass
+
+
+def play(*args):
+    """Play or Pause"""
+    pg.press(['playpause'])
+    pass
+
+def stop(*args):
+    pass
+
